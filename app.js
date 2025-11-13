1ede48f893395019ac44e763bb5f9cc75e1897a9
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-11-13 20:31:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-console.log("APP.JS ĐANG CHẠY");
-
-const p = document.createElement("p");
-p.textContent = "🟩 app.js đã chạy";
-p.style.color = "blue";
-p.style.fontWeight = "bold";
-document.body.appendChild(p);
+console.log("APP.JS ĐANG CHẠY OK");
